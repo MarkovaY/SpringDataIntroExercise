34a7eddf28cabaafe5521d3556e7ab485779dbb1
YanaMarkova <yana_markova@yahoo.com>
2023-07-12 17:05:11 +0200

    Initial commit

--- src/test/java/bg/softuni/springdataintroexercise/SpringDataIntroExerciseApplicationTests.java
+++ src/test/java/bg/softuni/springdataintroexercise/SpringDataIntroExerciseApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringDataIntroExerciseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
