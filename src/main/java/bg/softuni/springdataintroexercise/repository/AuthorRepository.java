8f8d99d254fe5a74aef22e579be7ab7e2a9e97a7
YanaMarkova <yana_markova@yahoo.com>
2023-07-19 14:02:59 +0200

    Initial commit

--- src/main/java/bg/softuni/springdataintroexercise/repository/AuthorRepository.java
+++ src/main/java/bg/softuni/springdataintroexercise/repository/AuthorRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
-    @Query("SELECT a FROM Author a ORDER BY a.books.size DESC")
+    @Query(value = "SELECT a FROM Author AS a ORDER BY a.books.size DESC")
     List<Author> findAllByBooksSizeDESC();
 }
